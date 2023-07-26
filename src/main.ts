045aabacef2933eab52aa41b0970779d036924fa
Ines Almeida <britalmeida@gmail.com>
2023-07-26 16:14:26 +0200

    some really important code

--- src/main.ts
+++ src/main.ts
@@ -8,11 +8,15 @@ function draw() {
   ui.addImage(0, 0, 800, 600, bgTextureID);
   ui.addImage(400-16, 300-16, 32, 32, eggTextureID);
 
+  ui.addImage(400, 300, 14 * 7, 14, coinTextureID);
+
   ui.draw();
 }
 
 
-let canvas = document.querySelector<HTMLCanvasElement>('#canvas-view');
+let canvas = document.querySelector<HTMLCanvasElement>('#canvas-view')!;
 let uiRenderer: UIRenderer = new UIRenderer(canvas, draw);
 let bgTextureID: WebGLTexture = uiRenderer.loadImage('/plates/background.png');
 let eggTextureID: WebGLTexture = uiRenderer.loadImage('/sprites/egg.png');
+
+let coinTextureID: WebGLTexture = uiRenderer.loadImage('/sprites/coin_spinning.png');
