e0eded9948b2217a512ec6bdeeed8cf7700c14a8
Inês Almeida <britalmeida@gmail.com>
2025-04-12 22:17:40 +0200

    Continuation of WIP Setup upgrade for development and packaging
    Improvements to the configuration, but the projects isn't running due to an error in shader code.
    Compiles with tsc and lints with ESLint (with errors).
    
    Improved TS configuration.
    Reworked ESLint setup in a similar way to uirenderer-canvas, but did not double check the resulting configuration.
    
    Added missing dev dependencies that were causing import errors in configuration files.
    - typescript-eslint is needed to get those two things working together.
    - @eslint/js has the recommended JS linting configuration to use as base for the rest.
    
    Updated dependencies.

--- eslint.config.js
+++ eslint.config.js
@@ -1,63 +1,61 @@
-import js from "@eslint/js";
-
-// Temp FlatCompat: TS does not yet support Flatconfig.
-// When it does, remove the "Temp FlatCompat" code and uncomment the rest.
+import globals from "globals";
+import eslint_js from "@eslint/js";
+import eslint_ts from "typescript-eslint";
 import tsParser from "@typescript-eslint/parser";
 import tsPlugin from "@typescript-eslint/eslint-plugin";
 
-// Temp FlatCompat
-import path from 'path';
-import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-import { FlatCompat } from '@eslint/eslintrc';
-const eslintrc = new FlatCompat({
-  baseDirectory: __dirname
-})
-// ~Temp FlatCompat
-
-export default [
+export default eslint_ts.config(
 
   // Base configurations being extended.
-  js.configs.recommended,
-  
-  // Temp FlatCompat
-  ...eslintrc.plugins("@typescript-eslint"),
-  ...eslintrc.extends(
-    "plugin:@typescript-eslint/strict-type-checked",
-    "plugin:@typescript-eslint/stylistic-type-checked",
-  ),
-  ...eslintrc.config({
-    parserOptions: {
-      project: ["./tsconfig.json"],
-    },
-    ignorePatterns: ["dist/*", "**/*.js"],
-  }),
-  // ~Temp FlatCompat
-  //tsPlugin.configs.strict-type-checked,
-  //tsPlugin.configs.stylistic-type-checked,
+  eslint_js.configs.recommended,
+
+  // Global ignores. These files will not be linted at all.
+  {
+    name: "eggscape/global-ignores",
+    ignores: ["types/", "dist/", ".idea/"],
+  },
+
+  // Base configurations which will match .js and .ts files.
+  // Subsequent configurations can affect the same files and will effectively extend these.
+  {
+    name: "uirenderer-canvas/js_fallback",
+    files: ["**/*.js", "**/*.mjs"],
+    plugins: { eslint_js },
+    extends: [
+      eslint_js.configs.recommended
+    ]
+  },
+  {
+    name: "eggscape/browser-globals",
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: { globals: globals.browser }
+  },
 
   // TypeScript linting.
   {
+    name: "eggscape/ts-linting",
     files: ["src/**/*.ts"],
 
+    extends: [
+      eslint_ts.configs.strictTypeChecked,
+      eslint_ts.configs.stylisticTypeChecked,
+    ],
     languageOptions: {
       parser: tsParser,
       parserOptions: {
         project: ["./tsconfig.json"],
       }
     },
-    //plugins: {
-    //  tsPlugin,
-    //},
+    plugins: {
+      tsPlugin,
+    },
     rules: {
       ...tsPlugin.configs['eslint-recommended'].overrides[0].rules,
     },
-
 
     linterOptions: {
       noInlineConfig: true,
     },
   }
 
-];
+);
